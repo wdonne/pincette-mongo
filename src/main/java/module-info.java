0ec68cf1cb2d71b1b164bde5b0d7a1a9769347e0
Werner Donné <werner.donne@pincette.biz>
2020-11-27 12:37:00 +0100

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module net.pincette.mongo {
+  requires java.json;
+  requires net.pincette.json;
+  requires java.logging;
+  requires org.mongodb.bson;
+  requires net.pincette.common;
+  requires org.reactivestreams;
+  requires net.pincette.rs;
+  requires org.mongodb.driver.core;
+  requires org.mongodb.driver.reactivestreams;
+  requires jslt;
+  exports net.pincette.mongo;
+}
